f099952e677abe407a327f6f1cca0d2ebc2db2ec
Martin Budinský <martinbudinsky3@gmail.com>
2022-06-24 11:52:26 +0200

    Fix user middleware

--- routes/web.php
+++ routes/web.php
@@ -38,7 +38,7 @@ Route::group(['prefix' => 'api', 'middleware' => 'auth:sanctum'], function () {
     Route::prefix('admin')->group(function () {
         Route::prefix('users')->group(function () {
             Route::get('/', [UserController::class, 'index'])->middleware('can:administrate');
-            Route::get('{user}/', [UserController::class, 'show'])/*->middleware('can:show', '\App\Models\User')*/; // MIDDLEWARE NOT WORKING
+            Route::get('{user}/', [UserController::class, 'show'])->middleware('can:show,user');
         });
     });
 });
